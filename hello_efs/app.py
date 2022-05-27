239c7d803f0631085ee40e3714350d15ae7101ac
Thomas Ricketts <rickettsdev@gmail.com>
2022-05-27 12:38:11 -0400

    Works in Lambda console. Requires manually adding VPC Gateway Endpoint, and S3 Permissions for Lambda Exe Role.

--- hello_efs/app.py
+++ hello_efs/app.py
@@ -1,17 +1,32 @@
 import json
 import boto3
 
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 BUCKET_NAME = "mysteriousmeat.data"
 FILE_NAME = "thirdPlant/pageData.json"
 
-def lambda_handler(event, context):
-    # s3 = boto3.resource('s3')
+region = "us-east-2"
+s3 = boto3.client('s3')
 
-    # content_object = s3.Object(BUCKET_NAME, FILE_NAME)
-    # file_content = content_object.get()['Body'].read().decode('utf-8')
-    # json_content = json.loads(file_content)
-    # print(json_content['Details'])
+def lambda_handler(event, context):
+    logger.info("s3 client created.")
+    # Retrieve the list of existing buckets
+    response = s3.list_buckets()
+
+    bucket_list = []
+
+    # Output the bucket names
+    logger.info('Existing buckets:')
+    for bucket in response['Buckets']:
+        bucket_list.append(bucket["Name"])
+        logger.info(f'  {bucket["Name"]}')
     return {
         "statusCode": 200,
+        "body": {
+            "bucket_list": bucket_list
+        }
     }
 
